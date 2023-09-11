278b1019db1f48128c39b8f8b31b52902db9eece
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-11 16:15:25 +0530

    JSX limitation and solution

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
-import React, { useState } from 'react';
-import UserDetailList from './components/UserDetails/UserDetailList/UserDetailList';
-import UserInput from './components/UserDetails/UserInput/UserInput';
+import React, { useState, Fragment } from 'react';
 
-import './App.css';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-const App = () => {
-  const [UserDetails, setUserDetails] = useState([
-    { text: 'aakarshan', id: 'g1',number:23 },
-  
-  ]);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (enteredUsername, enteredAge) => {
-    setUserDetails((prevUsers) => {
-      const updatedUsers = [...prevUsers];
-      updatedUsers.unshift({
-        text: enteredUsername,
-        number: enteredAge, // Add the age field
-        id: Math.random().toString(),
-      });
-      return updatedUsers;
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
-  
-  const deleteItemHandler = UserId => {
-    setUserDetails(prevUsers => {
-      const updatedUsers = prevUsers.filter(User => User.id !== UserId);
-      return updatedUsers;
-    });
-  };
-
-  let content = (
-    <p style={{ textAlign: 'center' }}>No Users found. Maybe add one?</p>
-  );
-
-  if (UserDetails.length > 0) {
-    content = (
-      <UserDetailList items={UserDetails} onDeleteItem={deleteItemHandler} />
-    );
-  }
 
   return (
-    <div>
-      <section id="goal-form">
-        <UserInput onAddUser={addUserHandler} />
-      </section>
-      <section id="Users">
-        {content}
-        {/* {UserDetails.length > 0 && (
-          <UserDetailList
-            items={UserDetails}
-            onDeleteItem={deleteItemHandler}
-          />
-        ) // <p style={{ textAlign: 'center' }}>No Users found. Maybe add one?</p>
-        } */}
-      </section>
-    </div>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
   );
-};
+}
 
 export default App;
